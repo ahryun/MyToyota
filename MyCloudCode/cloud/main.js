106c72a172099efdc4912bfb05b696c8ef9df66c
Ahryun Moon <ansdkfus13@gmail.com>
2014-11-07 22:35:16 -0800

    cloud code added

--- MyCloudCode/cloud/main.js
+++ MyCloudCode/cloud/main.js
@@ -1,6 +1,10 @@
+var userInfoURI = "https://api-jp-t-itc.com/GetUserInfo"
+var vehicleModelListURI = "https://api-jp-t-itc.com/GetVehicleModelList"
+var vehicleSpecURI = "https://api-jp-t-itc.com/GetVehicleSpec"
+var vehicleInfoURI = "https://api-jp-t-itc.com/GetVehicleInfo"
+var mapMatchingVehicleInfoURI = "https://api-jp-t-itc.com/GetVehicleInfoMM"
+var statisticsURI = "https://api-jp-t-itc.com/GetStatisticsInfo"
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
