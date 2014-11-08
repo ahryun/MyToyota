64faf857e3f3f03a09f669ed2414824c4fa580d1
Ahryun Moon <ansdkfus13@gmail.com>
2014-11-08 01:31:41 -0800

    start journey function added

--- MyCloudCode/cloud/main.js
+++ MyCloudCode/cloud/main.js
@@ -1,16 +1,17 @@
-var userInfoURI = "https://api-jp-t-itc.com/GetUserInfo"
-var vehicleModelListURI = "https://api-jp-t-itc.com/GetVehicleModelList"
-var vehicleSpecURI = "https://api-jp-t-itc.com/GetVehicleSpec"
-var vehicleInfoURI = "https://api-jp-t-itc.com/GetVehicleInfo"
-var mapMatchingVehicleInfoURI = "https://api-jp-t-itc.com/GetVehicleInfoMM"
-var statisticsURI = "https://api-jp-t-itc.com/GetStatisticsInfo"
+var userInfoURI = "https://api-jp-t-itc.com/GetUserInfo";
+var vehicleModelListURI = "https://api-jp-t-itc.com/GetVehicleModelList";
+var vehicleSpecURI = "https://api-jp-t-itc.com/GetVehicleSpec";
+var vehicleInfoURI = "https://api-jp-t-itc.com/GetVehicleInfo";
+var mapMatchingVehicleInfoURI = "https://api-jp-t-itc.com/GetVehicleInfoMM";
+var statisticsURI = "https://api-jp-t-itc.com/GetStatisticsInfo";
+var developerKey = '3552b02ecebf';
 
-Parse.Cloud.define("getOdometerReadingInMile", function(request, response) {
+Parse.Cloud.define("startJourney", function(request, response) {
   return Parse.Cloud.httpRequest({
         method: 'POST',
-        url: 'https://api-jp-t-itc.com/GetVehicleInfo',
+        url: vehicleInfoURI,
         params: {
-            developerkey    : '3552b02ecebf',
+            developerkey    : developerKey,
             responseformat  : 'json',
             vid             : request.vid,
         },
@@ -18,6 +19,20 @@ Parse.Cloud.define("getOdometerReadingInMile", function(request, response) {
             'Content-Type': 'application/json;charset=utf-8',
         },
         success: function(httpResponse) {
+            var query = new Parse.Query(Parse.User);
+            query.get(request.uid, {
+                success: function(user) {
+                    // The object was retrieved successfully.
+                    var Trip = Parse.Object.extend("Trip");
+                    var trip = new Trip();
+                    trip.set();
+                    user.set();
+                },
+                error: function(object, error) {
+                    // The object was not retrieved successfully.
+                    // error is a Parse.Error with an error code and message.
+                }
+            });
             response.success('Successfully retrieved: ' + httpResponse);
         },
         error: function(error) {
