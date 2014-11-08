59c36fa5d2c25fe2840fd4d7b040f750ad120a0d
Ahryun Moon <ansdkfus13@gmail.com>
2014-11-07 22:08:03 -0800

    added Xcode files

---
+++ MyToyota/MyToyota/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyToyota
+//
+//  Created by Jasper Sone on 11/7/14.
+//  Copyright (c) 2014 Papafish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
