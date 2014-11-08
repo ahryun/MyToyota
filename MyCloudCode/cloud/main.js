a9133811251a232129a1674bb5ff3589c114c91a
Ahryun Moon <ansdkfus13@gmail.com>
2014-11-08 01:08:26 -0800

    added REST api call to cloud code

--- MyCloudCode/cloud/main.js
+++ MyCloudCode/cloud/main.js
@@ -5,6 +5,23 @@ var vehicleInfoURI = "https://api-jp-t-itc.com/GetVehicleInfo"
 var mapMatchingVehicleInfoURI = "https://api-jp-t-itc.com/GetVehicleInfoMM"
 var statisticsURI = "https://api-jp-t-itc.com/GetStatisticsInfo"
 
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+Parse.Cloud.define("getOdometerReadingInMile", function(request, response) {
+  return Parse.Cloud.httpRequest({
+        method: 'POST',
+        url: 'https://api-jp-t-itc.com/GetVehicleInfo',
+        params: {
+            developerkey    : '3552b02ecebf',
+            responseformat  : 'json',
+            vid             : request.vid,
+        },
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+        },
+        success: function(httpResponse) {
+            response.success('Successfully retrieved: ' + httpResponse);
+        },
+        error: function(error) {
+            response.error("Something went wrong in query.... <<<< " + error);
+        },
+  });
 });
