c03234a69823b12fcd8701b9b1f0e5ffd067f879
Ahryun Moon <ansdkfus13@gmail.com>
2014-11-08 04:42:40 -0800

    added a few controllers

---
+++ MyToyota/MyToyota/MapViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MapViewController.swift
+//  MyToyota
+//
+//  Created by Jasper Sone on 11/8/14.
+//  Copyright (c) 2014 Papafish. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class MapViewController: UIViewController {
+
+    var mappedCar: PFObject!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
